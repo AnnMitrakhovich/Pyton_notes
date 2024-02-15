5d7fe47e49ea885e7519ceb886b1a2de18b67917
Ann <ann.mitrakhovich@gmail.com>
2024-02-15 22:07:35 +0300

    add converter

--- ConverterToJSON.py
+++ ConverterToJSON.py
@@ -1,9 +1,6 @@
 import json
-
-import view
-from note import Note
-
-
+import View
+import Note
 
 class ConverterToJSON:
 
@@ -65,13 +62,13 @@ class ConverterToJSON:
 
         notes_json = json.dumps(json_strings_list, indent=4, ensure_ascii=False, sort_keys=False, default=str)
 
-        with open(self.filename, "w", encoding='utf-8') as my_file:
+        with open(self.file_name, "w", encoding='utf-8') as my_file:
             my_file.write(notes_json)
 
     def read_json(self):
         notes_list = list()
         try:
-            with open(self.filename, "r", encoding='utf-8') as my_file:
+            with open(self.file_name, "r", encoding='utf-8') as my_file:
                 notes_json = my_file.read()
             data = json.loads(notes_json)
             data.sort(key=lambda x: x['date'])
